3120d5f9e9cb4595f923042911adc7775c778d20
Oskar Szura <oskar.szura@gmail.com>
2019-12-13 18:19:48 +0100

    HOME-268 - Fix deployment scripts (#17)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-curl --header "X-Cear-Auth: $CEAR_KEY" -d "SHPANEL_V=$APP_VERSION" -X POST $CEAR_URL
+curl --header "X-Cear-Auth: $CEAR_KEY" --header "X-Cear-Params: SHPANEL_V=$APP_VERSION" -X POST $CEAR_URL
