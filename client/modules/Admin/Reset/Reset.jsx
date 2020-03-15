99e4a924f1d758ba87be78dcac697a350107681c
Oskar Szura <oskar.szura@gmail.com>
2020-03-15 15:39:29 +0100

    HOME-312 - Add dialog warning before clearing to default state

--- client/modules/Admin/Reset/Reset.jsx
+++ client/modules/Admin/Reset/Reset.jsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Reset = (props: Props) => {
   const { reset } = props;
-  const [ isDialogShown, setIsDialogShown ] = useState(false);
+  const [isDialogShown, setIsDialogShown] = useState(false);
 
   return (
     <div className="shp-admin__card gc-card gc-card--default gc-panel">
@@ -19,30 +19,25 @@ const Reset = (props: Props) => {
       <div className="gc-panel__footer">
         <Button
           className="gc-btn gc-btn--danger"
-            onClick={() => setIsDialogShown(true)}
+          onClick={() => setIsDialogShown(true)}
         >
           Reset
         </Button>
       </div>
-      { isDialogShown && (
+      {isDialogShown && (
         <Dialog>
-          <article className='gc-panel'>
-            <header className='gc-panel__title'>
-              Reset confirmation
-            </header>
-            <div  className='gc-panel__content'>
+          <article className="gc-panel">
+            <header className="gc-panel__title">Reset confirmation</header>
+            <div className="gc-panel__content">
               Are you sure you want to restore default settings?
             </div>
-            <div  className='gc-panel__footer'>
-              <Button
-                onClick={() => reset()}
-                className='gc-btn--danger'
-              >
+            <div className="gc-panel__footer">
+              <Button onClick={() => reset()} className="gc-btn--danger">
                 Restore
-              </Button>{" "}
+              </Button>{' '}
               <Button
                 onClick={() => setIsDialogShown(false)}
-                className='gc-btn--info'
+                className="gc-btn--info"
               >
                 Cancel
               </Button>
