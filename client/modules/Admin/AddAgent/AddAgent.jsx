1cc83e6d43ccbcf2110fecef086ee469310e1402
Oskar Szura <oskar.szura@gmail.com>
2020-03-17 08:18:54 +0100

    HOME-309 - Validation to 'add agent' form

--- client/modules/Admin/AddAgent/AddAgent.jsx
+++ client/modules/Admin/AddAgent/AddAgent.jsx
@@ -1,5 +1,8 @@
 // @flow
 import React, { useState, useCallback } from 'react';
+import { Input, Validation } from 'graphen';
+import classNames from 'classnames';
+import * as queries from './queries';
 
 type Props = {
   addAgent: (string, string, string, string) => void,
@@ -12,12 +15,51 @@ const AddAgent = (props: Props) => {
   const [agentName, setAgentName] = useState('');
   const [agentType, setAgentType] = useState('');
 
+  const [idValidation, setIDValidation] = useState(null);
+  const [ipValidation, setIPValidation] = useState(null);
+  const [nameValidation, setNameValidation] = useState(null);
+  const [typeValidation, setTypeValidation] = useState(null);
+
+  const handleIDChange = useCallback(
+    event => {
+      const id = event.target.value;
+      setAgentID(id);
+      setIDValidation(queries.validateID(id));
+    },
+    [agentID, setAgentID]
+  );
+  const handleIPChange = useCallback(
+    event => {
+      const ip = event.target.value;
+      setAgentIP(ip);
+      setIPValidation(queries.validateIP(ip));
+    },
+    [agentIP, setAgentIP]
+  );
+  const handleNameChange = useCallback(
+    event => {
+      const name = event.target.value;
+      setAgentName(name);
+      setNameValidation(queries.validateName(name));
+    },
+    [agentName, setAgentName]
+  );
+  const handleTypeChange = useCallback(
+    event => {
+      const type = event.target.value;
+      setAgentType(type);
+      setTypeValidation(queries.validateType(type));
+    },
+    [agentType, setAgentType]
+  );
   const submit = useCallback(() => {
-    addAgent(agentID, agentIP, agentName, agentType);
-    setAgentID('');
-    setAgentIP('');
-    setAgentName('');
-    setAgentType('');
+    if (idValidation && ipValidation && nameValidation && typeValidation) {
+      addAgent(agentID, agentIP, agentName, agentType);
+      setAgentID('');
+      setAgentIP('');
+      setAgentName('');
+      setAgentType('');
+    }
   }, [
     addAgent,
     agentID,
@@ -28,56 +70,124 @@ const AddAgent = (props: Props) => {
     setAgentIP,
     setAgentName,
     setAgentType,
+    idValidation,
+    ipValidation,
+    nameValidation,
+    typeValidation,
   ]);
+
+  const idInputClasses = classNames('tst-add-agent-id gc-input__field', {
+    'gc-input__field--danger': idValidation === false,
+    'gc-input__field--success': idValidation === true,
+  });
+  const idInput = (
+    <div className="gc-input gc-input--full">
+      <label htmlFor="add-agent-id" className="gc-input__label">
+        ID
+      </label>
+      <input
+        id="add-agent-id"
+        className={idInputClasses}
+        value={agentID}
+        onChange={handleIDChange}
+      />
+    </div>
+  );
+  const idInputComponent =
+    idValidation !== false ? (
+      idInput
+    ) : (
+      <Validation type="danger" message="ID should be text">
+        {idInput}
+      </Validation>
+    );
+
+  const ipInputClasses = classNames('tst-add-agent-ip gc-input__field', {
+    'gc-input__field--danger': ipValidation === false,
+    'gc-input__field--success': ipValidation === true,
+  });
+  const ipInput = (
+    <div className="gc-input gc-input--full">
+      <label htmlFor="add-agent-ip" className="gc-input__label">
+        IP
+      </label>
+      <input
+        id="add-agent-ip"
+        className={ipInputClasses}
+        value={agentIP}
+        onChange={handleIPChange}
+      />
+    </div>
+  );
+  const ipInputComponent =
+    ipValidation !== false ? (
+      ipInput
+    ) : (
+      <Validation type="danger" message="IP should be text">
+        {ipInput}
+      </Validation>
+    );
+
+  const nameInputClasses = classNames('tst-add-agent-name gc-input__field', {
+    'gc-input__field--danger': nameValidation === false,
+    'gc-input__field--success': nameValidation === true,
+  });
+  const nameInput = (
+    <div className="gc-input gc-input--full">
+      <label htmlFor="add-agent-name" className="gc-input__label">
+        Name
+      </label>
+      <input
+        id="add-agent-name"
+        className={nameInputClasses}
+        value={agentName}
+        onChange={handleNameChange}
+      />
+    </div>
+  );
+  const nameInputComponent =
+    nameValidation !== false ? (
+      nameInput
+    ) : (
+      <Validation type="danger" message="Name should be text">
+        {nameInput}
+      </Validation>
+    );
+
+  const typeInputClasses = classNames('tst-add-agent-type gc-input__field', {
+    'gc-input__field--danger': typeValidation === false,
+    'gc-input__field--success': typeValidation === true,
+  });
+  const typeInput = (
+    <div className="gc-input gc-input--full">
+      <label htmlFor="add-agent-type" className="gc-input__label">
+        Type
+      </label>
+      <input
+        id="add-agent-type"
+        className={typeInputClasses}
+        value={agentType}
+        onChange={handleTypeChange}
+      />
+    </div>
+  );
+  const typeInputComponent =
+    typeValidation !== false ? (
+      typeInput
+    ) : (
+      <Validation type="danger" message="Type should be text">
+        {typeInput}
+      </Validation>
+    );
 
   return (
     <div className="shp-admin__card gc-card gc-card--default gc-panel">
       <div className="gc-panel__title">Add agent</div>
       <div className="gc-panel__content">
-        <div className="gc-input gc-input--full">
-          <label htmlFor="add-agent-id" className="gc-input__label">
-            Agent ID
-          </label>
-          <input
-            id="add-agent-id"
-            className="tst-add-agent-id gc-input__field"
-            value={agentID}
-            onChange={event => setAgentID(event.target.value)}
-          />
-        </div>
-        <div className="gc-input gc-input--full">
-          <label htmlFor="add-agent-ip" className="gc-input__label">
-            Agent IP
-          </label>
-          <input
-            id="add-agent-ip"
-            className="tst-add-agent-ip gc-input__field"
-            value={agentIP}
-            onChange={event => setAgentIP(event.target.value)}
-          />
-        </div>
-        <div className="gc-input gc-input--full">
-          <label htmlFor="add-agent-name" className="gc-input__label">
-            Agent Name
-          </label>
-          <input
-            id="add-agent-name"
-            className="tst-add-agent-name gc-input__field"
-            value={agentName}
-            onChange={event => setAgentName(event.target.value)}
-          />
-        </div>
-        <div className="gc-input gc-input--full">
-          <label htmlFor="add-agent-type" className="gc-input__label">
-            Agent Type
-          </label>
-          <input
-            id="add-agent-type"
-            className="tst-add-agent-type gc-input__field"
-            value={agentType}
-            onChange={event => setAgentType(event.target.value)}
-          />
-        </div>
+        {idInputComponent}
+        {ipInputComponent}
+        {nameInputComponent}
+        {typeInputComponent}
       </div>
       <div className="gc-panel__footer">
         <button
---
+++ client/modules/Admin/AddAgent/queries.js
@@ -0,0 +1,7 @@
+export const validateID = (id: string): boolean => {
+  console.log(id);
+  return id !== '';
+};
+export const validateName = (name: string): boolean => name !== '';
+export const validateType = (type: string): boolean => type !== '';
+export const validateIP = (ip: string): boolean => ip !== '';
