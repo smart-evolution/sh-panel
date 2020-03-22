f15ea4939b2d10077c0cf1b285bc89ca04b484d5
Oskar Szura <oskar.szura@gmail.com>
2020-03-22 21:50:40 +0100

    HOME-315 - Fix 'send alert' switch glitch

--- client/components/Switch/index.jsx
+++ client/components/Switch/index.jsx
@@ -11,12 +11,16 @@ type Props = {
 const Switch = (props: Props) => {
   const { isOn, onToggle, className } = props;
 
+  /* eslint-disable jsx-a11y/no-static-element-interactions */
   return (
     <label
+      htmlFor="send-alert-switch"
+      role="button"
       className={`gc-switch ${className}`}
       onClick={onToggle}
     >
       <input
+        id="send-alert-switch"
         className="gc-switch__input"
         type="checkbox"
         checked={isOn}
@@ -25,6 +29,7 @@ const Switch = (props: Props) => {
       <span className="gc-switch__slider" />
     </label>
   );
+  /* eslint-enable jsx-a11y/no-static-element-interactions */
 };
 
 Switch.defaultProps = {
